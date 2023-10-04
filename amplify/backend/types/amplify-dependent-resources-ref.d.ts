6d95bb2a75943a893945796e27f8ecfa84d9b14a
maira-samtek <maira@samtek.io>
2023-10-03 20:12:54 -0400

    deleted extra code

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,5 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "CustomDeploymentRule": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
     "apieff7a381": {
       "ApiId": "string",
       "ApiName": "string",
@@ -13,13 +8,6 @@ export type AmplifyDependentResourcesAttributes = {
   },
   "function": {
     "testcustomrules168ff630": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "testcustomrules6beecc59": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
